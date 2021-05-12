5137fa0ad525437ef29c9347c6767c3726e8a8a9
Jakub <jakub@jevlk.cz>
2021-05-12 17:07:22 +0200

    Add key functionality
    changes:
    - downloading data from the web instead of using local copy
    - add using request, pyjstat, collections
    
    status
    prints the first 3 and last 3 states by unemployment

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import json
+from pyjstat import pyjstat
+import requests
+from collections import OrderedDict
 
-pathtoFile = 'oecd.json'
-
-def loadFiles():
-    with open(pathtoFile) as f:
-        data = json.load(f)
-    return data
+def loadFile():
+    EXAMPLE_URL = 'https://json-stat.org/samples/oecd.json'
+    data = requests.get(EXAMPLE_URL)
+    results = pyjstat.from_json_stat(data.json(object_pairs_hook=OrderedDict))
+    return results
 
 def main():
-    print(loadFiles())
+    df = loadFile()
+    sortedData = df[0].sort_values(by='value')
+    print('countries with the lowest:')
+    print(sortedData[0:3])
+
+    print('countries with the highest:')
+    print(sortedData[-3:])
 
 if __name__ == "__main__":
     main()
